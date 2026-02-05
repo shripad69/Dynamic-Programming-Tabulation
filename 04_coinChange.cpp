b7498ccb3bd30223739605f46038394d659f5e96
shripad_69 <shripadalgude@gmail.com>
2026-02-05 23:49:11 +0530

    coin_change_modi

--- 04_coinChange.cpp
+++ 04_coinChange.cpp
@@ -9,38 +9,34 @@
 
 class Solution {
 public:
-
     int coinChange(vector<int>& coins, int amount) {
-
         int n = coins.size();
+        
+        // Using amount + 1 as a safe "infinity"
+        vector <vector <int>> dp (n + 1, vector <int> (amount + 1, amount + 1));
 
-        vector <vector <int>> dp (n + 1, vector <int> (amount + 1, INT_MAX));
-
-        // initilize the base case
-        dp[0][0] = 1;
-        for (int j = 1; j <= amount; j++) {
-            dp[0][i] = 0; 
+        // Base case: 0 coins needed to make amount 0
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
         }
 
         // filling table
-        for (int i = 1; <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= amount; j++) {
+                
+                // 1. Logic: Don't take and go back (Skip current coin)
+                dp[i][j] = dp[i - 1][j];
 
-                // take and stay
-                if (am - coins[j - 1] >= 0) {
-                    dp[i][j] = min(dp[i][j], 1 + dp[i][am - coins[j - 1]]);
+                // 2. Logic: Take and stay (Use current coin, stay on same row)
+                // Use i-1 to pick the current coin from the vector
+                if (j - coins[i - 1] >= 0) {
+                    dp[i][j] = min(dp[i][j], 1 + dp[i][j - coins[i - 1]]);
                 }
-
-                // take and go back
-                if (am - coins[j - 1] >= 0) {
-                    dp[i][j] = min(dp[i][j], 1 + dp[i - 1][am - coins[j - 1]]);
-                }
-
-                // dont take and go back
-                dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+                
             }
         }
 
-        return dp[n][amount];
+        if (dp[n][amount] <= amount) return dp[n][amount];
+        return -1;
     }
 };
